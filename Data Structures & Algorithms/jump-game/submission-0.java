7d4d3fdba805efaa7e486c085f5da18b9eaa6d25
Kaiwen Yao <114751518+kaiwenyao@users.noreply.github.com>
2026-09-01 15:11:09 +0100

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > reach) {
+                return false;
+            }
+            reach = Math.max(i + nums[i], reach);
+            
+            if (reach >= n - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
